e824534ec3162ac3d28085b98359bf0785033eeb
dat.nguyen <contact@datvannguyen.net>
2019-03-11 00:35:08 +0700

    done

--- worker/VipLike.php
+++ worker/VipLike.php
@@ -3,7 +3,6 @@
 require __DIR__ . "/DataLayer.php" ;
 require __DIR__. "/libs/Request.php";
 
-
 class Task extends Threaded {
 
     public $data;
@@ -16,41 +15,76 @@ class Task extends Threaded {
 
     public function run()
     {
-        $this->__reactionPost($this->data["post_id"], $this->data["reaction"], $this->data["token"]);
+        // $this->__reactionPost($this->data["post_id"], $this->data["reaction"], $this->data["token"]);
+        $this->test();
+    }
+
+    public function test()
+    {
+        //echo __DIR__;die;
+        
     }
 
     public function __reactionPost($post_id, $reaction, $token)
     {
         //$request = new Request();
         $url = "https://graph.facebook.com/{$post_id}/reactions?type={$reaction}&access_token={$token}&method=post";
-        $this->request->get($url);
+        $response = json_decode($this->request->get($url), true);
+        if()
     }
-}
-//$start_time = microtime(TRUE);
-$model = new DataLayer();
-$p = new Pool(10);
-$processes = $model->getActiveProcesses();
-if(count($processes) > 0)
-{
-    foreach($processes as $process)
+
+    public function updateTokenDie($token_id)
     {
-        $datas = $model->getRandByProcessId($process["id"], $process["quantity_per_cron"]);
-        if(count($datas) > 0)
+        require __DIR__ . "/vendor/autoload.php";
+        $connection = new PDO('mysql:host=localhost;dbname=viplikedat', 'root', '');
+        $h = new \ClanCats\Hydrahon\Builder('mysql', function($query, $queryString, $queryParameters) use($connection)
         {
-            $request = new Request();
-            foreach($datas as $data)
+            $statement = $connection->prepare($queryString);
+            $statement->execute($queryParameters);
+            
+            if ($query instanceof \ClanCats\Hydrahon\Query\Sql\FetchableInterface)
             {
-                $p->submit(new Task($data, $request));
-                $model->updateTokenProcessMap($data["id"], ["status" => 1, "is_runned" => 1]);
+                return $statement->fetchAll(\PDO::FETCH_ASSOC);
             }
-        }
-        else
-        {
-            $model->updateProcess($process["id"], ["is_done" => 1]);
-        }
+        });
+        $tokens = $h->table("tokens");
+        $tokens->update(["status" => 0])->where("id", $token_id)->execute();
     }
 }
+
+$p = new Pool(1);
+$p->submit(new Task(2, 2));
 while($p->collect());
 $p->shutdown();
+
+//$start_time = microtime(TRUE);
+
+// $model = new DataLayer();
+// $p = new Pool(10);
+// $processes = $model->getActiveProcesses();
+// if(count($processes) > 0)
+// {
+//     foreach($processes as $process)
+//     {
+//         $datas = $model->getRandByProcessId($process["id"], $process["quantity_per_cron"]);
+//         if(count($datas) > 0)
+//         {
+//             $request = new Request();
+//             foreach($datas as $data)
+//             {
+//                 $p->submit(new Task($data, $request));
+//                 $model->updateTokenProcessMap($data["id"], ["status" => 1, "is_runned" => 1]);
+//             }
+//         }
+//         else
+//         {
+//             $model->updateProcess($process["id"], ["is_done" => 1]);
+//         }
+//     }
+// }
+// while($p->collect());
+// $p->shutdown();
+
+
 //$end_time = microtime(TRUE);
 //echo $end_time - $start_time;
